edce115a74c54471a2fef95add6764592493f42c
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2021-10-07 14:19:39 -0500

    .

--- ComputerOpponent.java
+++ ComputerOpponent.java
@@ -13,36 +13,44 @@ public class ComputerOpponent {
 		
 		//Guaranteed win for computer immediately
 	    	{"OOOOO", 1000000000},
-		//Guaranteed win for player immediately
+		//Guaranteed win for player in 1 moves
 			{"XXXX_", -100000000},
 	    	{"XXX_X", -100000000},
 			{"XX_XX", -100000000},
 			{"X_XXX", -100000000},
 			{"_XXXX", -100000000},
 		//Guaranteed win for computer in 2 moves
-		{"OOOO_", 1000},
-		{"OOO_O", 1000},
-		{"OO_OO", 1000},
-		{"O_OOO", 1000},
-		{"_OOOO", 1000},
-        {"_XX(?=(_))", -4},
-        {"_XX(?=(O))", -2},
-        {"OXX(?=(_))", -2},
-        {"OXX(?=(O))", -1},
-        {"__XXX(?=(_))", -100},
-        {"_XXX(?=(__))", -100},
-	    {"_XX_X(?=(_))", -100},
-        {"_X_XX(?=(_))", -100},
-		{"_OO(?=(_))", 3},
-		{"_OO(?=(X))", 2},
-	    {"XOO(?=(_))", 2},
-		{"XOO(?=(X))", 1},
-		{"__OOO(?=(_))", 15},
-		{"_OOO(?=(__))", 15},
-	    {"_OO_O(?=(_))", 12},
-		{"_O_OO(?=(_))", 12},
-		{"OX", 1},
-		{"XO", 1}
+			{"_OOOO_",  20000000},
+		//Possible win for computer in 2 moves
+			{"OOO_O", 10000000},
+			{"OO_OO", 10000000},
+			{"O_OOO", 10000000},
+			{"_OOOO", 10000000},
+		//Possible win for player in 3 moves
+			{"_XXX__",-1000000},
+			{"__XXX_",-1000000},
+			{"_XX_X_",-1000000},
+			{"_X_XX_",-1000000},
+			{"_XXX_",-500000},
+		//Possible win for computer in 4 moves
+			{"_OOO__", 100000},
+			{"__OOO_", 100000},
+			{"_OO_O_", 100000},
+			{"_O_OO_", 100000},
+			{"_OOO_", 50000},
+		//Good moves for computer
+			{"__OO__", 1200},
+			{"_OO__", 1000},
+			{"__OO_", 1000},
+			{"XXO_", 500},
+			{"_OXX", 500},
+		//Barely disatvantageous situations for computer
+			{"__XX_", -1100},
+			{"_XX__", -1100},
+			{"OXO", -100},
+		//Don't go in random places
+			{"OX", 1},
+			{"XO", 1}
         //endregion
     }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Five-in-a-row
-Get five pieces in a row. Play against the computer
+Get five pieces in a row. Play against the computer<br>
+This is a just game of concentration, if you know the strategy ...<br><br>
+## How to play
+Click on any space to place a piece.
